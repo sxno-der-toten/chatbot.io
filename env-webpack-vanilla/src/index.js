056917eeb7048b9f6c3dbb26058b6e1bb87d45b0
youness <youness.benakkaf@coda-student.school>
2024-02-16 10:45:25 +0100

    m

--- env-webpack-vanilla/src/index.js
+++ env-webpack-vanilla/src/index.js
@@ -1,1 +1,14 @@
+import Router from './Router';
+import Home from './controllers/Home';
+
 import './index.scss';
+
+const routes = [{
+  url: '/',
+  controller: Home
+}, {
+  url: '/search',
+  controller: Search
+}];
+
+new Router(routes);

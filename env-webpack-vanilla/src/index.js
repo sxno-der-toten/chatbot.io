a6cde8e4d07398cbe45b58c4afd922c679ecffd2
sano <fawez.azzouk@coda-student.school>
2024-02-16 10:51:01 +0100

    scuze youhess

--- env-webpack-vanilla/src/index.js
+++ env-webpack-vanilla/src/index.js
@@ -1,14 +1,1 @@
-import Router from './Router';
-import Home from './controllers/Home';
-
 import './index.scss';
-
-const routes = [{
-  url: '/',
-  controller: Home
-}, {
-  url: '/search',
-  controller: Search
-}];
-
-new Router(routes);
